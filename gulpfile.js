d90be51e904ddde083bfb490ae0bfa2e162156ad
Jim Lawler <jimlawler@MacBook-Pro.local>
2018-04-29 11:36:02 -0400

    concat javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
-
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+var jsSources = [
+	'components/scripts/layout.js',
+	'components/scripts/jqloader.js'
+]
 gulp.task('log', function() {
 	gutil.log('hello');
+});
+gulp.task ('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
