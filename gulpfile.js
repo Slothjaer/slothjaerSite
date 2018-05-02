7ed7b73d592ee90d0888b6b6fd336167f4aa0c8b
Jim Lawler <jimlawler@MacBook-Pro.local>
2018-05-01 20:45:07 -0400

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 var jsSources = [
 	'components/scripts/layout.js',
 	'components/scripts/jqloader.js'
 ]
+var sassSources = ['components/sass/style.scss']
 gulp.task('log', function() {
 	gutil.log('hello');
 });
@@ -14,4 +16,15 @@ gulp.task ('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task ('sass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
 });
