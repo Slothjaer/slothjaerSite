294b622a701e40b82949aa800b7682d494b74961
Jim Lawler <jimlawler@MacBook-Pro.local>
2018-05-03 07:55:52 -0400

    added static file management

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
+//var env = process.env.NODE.ENV || 'development';
 var jsSources = [
 	'components/scripts/layout.js',
 	'components/scripts/jqloader.js'
 ]
-var sassSources = ['components/sass/style.scss']
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
 gulp.task('log', function() {
 	gutil.log('hello');
 });
@@ -16,6 +19,7 @@ gulp.task ('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task ('sass', function() {
@@ -27,4 +31,23 @@ gulp.task ('sass', function() {
 		}))
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload());
 });
+gulp.task ('watch', function () {
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['sass']);
+	gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task ('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task ('html', function () {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+})
+gulp.task('default', ['html', 'js', 'sass', 'connect', 'watch']);
